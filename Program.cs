747d3b38b2c76511d644e1eb8f6c544fc2fe871e
Adelaide <88384474+allytyson16@users.noreply.github.com>
2022-01-31 23:38:39 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace Udemyyyyy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+/* a program that prompts the user for an hourly pay rate. If the value entered is less than $7.50 
+         * or greater than $49.00, display an error message, otherwise display a message indicating that the rate is
+         * okay*/
+		
+		Console.WriteLine("What is your hourly pay rate? - ");
+		double hpr = double.Parse(Console.ReadLine());
+		
+		if(hpr < 7.50 || hpr > 49.00)
+		{
+			Console.WriteLine("Error");
+		}
+		else
+		{
+			Console.WriteLine("Okay");
+		}
+		
         }
     }
 }
